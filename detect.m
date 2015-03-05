be2846deb5937ab617c80e8495636aac31b6d8bc
SuryaTeja Cheedella <surya123shine@gmail.com>
2015-03-05 13:11:05 +0530

    minor modifications

--- detect.m
+++ detect.m
@@ -1,7 +1,7 @@
 negativeFolder= fullfile('C:\Users\Surya Teja Cheedella\Documents\MATLAB\carDetection\carData\trainImagesNeg')
 
 %initialising the variable detector with the xml file
-detector= vision.CascadeObjectDetector('carTraindata3.xml');
+detector= vision.CascadeObjectDetector('carTraindata4.xml');
 %reading an image file in current dir.
 img= imread('sun.png');
 %bounding box around detected object
--- imshowER.m
+++ imshowER.m
@@ -1,4 +1,7 @@
-for j= 1:100,
+negativeFolder= fullfile('C:\Users\Surya Teja Cheedella\Documents\MATLAB\carDetection\carData\trainImagesNeg');
+detector= vision.CascadeObjectDetector('carTraindata4.xml');
+
+for j= 1:20,
     img= imread(strcat('test-', num2str(j-1), '.pgm'));
     bbox= step(detector, img);
     figure,imshow(insertObjectAnnotation(img, 'rectangle', bbox,' '));
